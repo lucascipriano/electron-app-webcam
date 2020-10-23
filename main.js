4a4b8d4a4984aebda8c3d2df004d87699f8c4ca1
Lucas Cipriano <lucasncborges@gmail.com>
2020-10-23 00:25:37 -0300

    base project

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    
+
+});
+
